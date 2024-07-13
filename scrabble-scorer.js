22d2627f7c8ccc117f17a87a4fa79ba9d537054c
DyRoss5 <dyamondross5@gmail.com>
2024-07-13 10:23:25 -0500

    <Scrabble Scorer>

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -26,29 +26,104 @@ function oldScrabbleScorer(word) {
  
 	  }
 	}
+   
 	return letterPoints;
+
  }
 
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+   console.log("Let's play some scrabble!\n");
+
+   let userWord = input.question("Please enter a word to score: ");
+   console.log(`The score for ${userWord} is ${scorerPrompt(userWord)}`);
+   
 };
 
-let newPointStructure;
+let newPointStructure = transform(oldPointStructure);
 
-let simpleScorer;
+let simpleScorer = function (word) {
+   let score = 0;
+   word = word.toUpperCase();
 
-let vowelBonusScorer;
+   for (let i = 0; i < word.length; i++) {
+      score +=1;
+   }
+   return score;
+}
 
-let scrabbleScorer;
+let vowelBonusScorer = function (word) {
+   let score = 0;
+   let vow = "AEIOU";
 
-const scoringAlgorithms = [];
+   for ( let i = 0; i < word.length; i++) {
+      if (vow.includes(word[i].toUpperCase())) {
+         score +=3; 
+      } else {
+         score +=1;
+      }
+   }
+   
+   return score;
+};
 
-function scorerPrompt() {}
+let scrabbleScorer = function(word) {
+   word = word.toLowerCase();
 
-function transform() {};
+   let score = 0;
+   //let letter = " ";
+
+   for (let i = 0; i < word.length; i++) {
+      score += newPointStructure[word[i]]
+   }
+   
+   return score;
+}
+
+const scoringAlgorithms = [
+   simpleScore = {
+      name: "Simple Score",
+      description: "Each ltter is worth 1 point.",
+      scorerFunction: simpleScorer
+   },
+
+   vowelBonus = {
+      name: "Bonus Vowels",
+      description: "Vowels are 3pts, consonants are 1pt.",
+      scorerFunction: vowelBonusScorer
+   },
+
+   scrabble = {
+      name: "Scrabble",
+      description: "The traditional scoring algorithm",
+      scorerFunction: scrabbleScorer
+   }
+]
+      
+function scorerPrompt(word) {
+   console.log("Which scoring algorithm would you like to use?")
+
+   for(let i = 0; i < scoringAlgorithms.length; i++){
+   console.log(`${i} - ${scoringAlgorithms[i].name}: ${scoringAlgorithms[i].description}`)
+}
+
+let selectedArr = input.question('Enter 0, 1, or 2: ') 
+  return scoringAlgorithms[selectedArr].scorerFunction(word);
+   
+}
+
+function transform(oldPointStructure) {
+   let newObj = {};
+   for (key in oldPointStructure) {
+      keyArr = oldPointStructure[key];
+      for (let i = 0; i < keyArr.length; i++)
+        newObj[(keyArr[i].toLowerCase())] = Number(key); 
+   }
+   return newObj;
+      
+};
 
 function runProgram() {
    initialPrompt();
